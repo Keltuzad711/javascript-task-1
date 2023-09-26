efa1e748c9ba594e7c111c61ad5ffe2735392050
Keltuzad711 <145975373+Keltuzad711@users.noreply.github.com>
2023-09-26 15:22:23 +0400

    Add files via upload

---
+++ task1/task.js
@@ -0,0 +1,18 @@
+
+//task1
+//let a = Number(prompt("enter a") )
+//let b = Number (prompt("enter b"))
+//let area = a*b
+//alert ( "area= " + area)
+
+//task2
+//let year = Number(prompt("Enter year"))
+//let birthyear = Number(prompt("Enter birth year"))
+//let yourAge = year-birthyear
+//alert ("YourAge = " +yourAge)
+
+//task3
+//let kilometres = Number(prompt("Enter Km"))
+//const kmdividedm = 0.6214
+//let miles =kilometres*kmdividedm
+//alert ( kmdividedm + "miles" )
